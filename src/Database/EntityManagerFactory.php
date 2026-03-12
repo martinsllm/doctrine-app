ad16dc9c83f3d635bd03616afc49174e2d3d2512
martinsllm <lucaslm2010@gmail.com>
2026-03-11 21:37:50 -0300

    Using logs

--- src/Database/EntityManagerFactory.php
+++ src/Database/EntityManagerFactory.php
@@ -3,8 +3,11 @@
 namespace App\Database;
 
 use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Logging\Middleware;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
@@ -16,6 +19,11 @@ class EntityManagerFactory
             paths: [__DIR__ . '/..'],
             isDevMode: true,
         );
+
+        $consoleOutput = new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG);
+        $consoleLogger = new ConsoleLogger($consoleOutput);
+        $logMiddleware = new Middleware($consoleLogger);
+        $config->setMiddlewares([$logMiddleware]);
         
         $connection = DriverManager::getConnection([
             'driver' => 'pdo_sqlite',
